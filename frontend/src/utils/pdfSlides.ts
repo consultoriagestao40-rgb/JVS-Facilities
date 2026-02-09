a29098e3349eb162e12f79f9e9677842cc9f7a5a
Cristiano Silva <cristiano.godoi@gotmail.com>
2026-02-09 11:48:20 -0300

    feat: manual deploy altura

--- frontend/src/utils/pdfSlides.ts
+++ frontend/src/utils/pdfSlides.ts
@@ -82,7 +82,7 @@ export const renderQuemSomos = (doc: jsPDF, width: number, height: number) => {
     // Decoration (Subtle Curve)
     doc.setFillColor(COLORS.BG_CARD);
     doc.circle(width, 0, 140, 'F');
-    // Force Deploy Checkpoint: Final Release V51 (Fix Google Tag Build Error)
+    // Force Deploy Checkpoint: Final Release V52 (Add Meta Pixel)
 
     // HEADER
     const margin = 20;
